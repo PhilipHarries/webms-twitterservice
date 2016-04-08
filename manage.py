2e9fc0f0ed17eaa45058f96efeef96cd90d6453d
PhilipHarries <philip.harries.unix.consulting@gmail.com>
2016-04-08 16:49:58 +0100

    Fixed pep8 warnings

--- manage.py
+++ manage.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
 
 # Set the path
-import os, sys
+import os
+import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
 from flask.ext.script import Manager, Server
@@ -11,16 +12,12 @@ manager = Manager(app)
 
 # Turn on debugger by default and reloader
 manager.add_command("runserver", Server(
-    use_debugger = True,
-    use_reloader = True,
-    host = '0.0.0.0',
-    port = '5431'
+    use_debugger=True,
+    use_reloader=True,
+    host='0.0.0.0',
+    port='5431'
     )
 )
 
 if __name__ == "__main__":
     manager.run()
-
-
-
-
